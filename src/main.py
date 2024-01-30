17763adb0006658543bc05d263b2e4f6db433ad0
Ricardo Manzano <ricardoamsrams@gmail.com>
2024-01-30 12:14:20 -0500

    Add main function

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from modules.test import test1
+import sys 
+  
+# Prints the list of directories that the  
+# interpreter will search for the required module.  
+print(sys.path)
+print("main")
+print(test1())
